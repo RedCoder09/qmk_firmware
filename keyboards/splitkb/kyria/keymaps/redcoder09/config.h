42111b6f1335bf138342a23d5fc46e4287891cf2
RedCoder09 <energeticmatey09@gmail.com>
2025-07-12 16:23:45 -0500

    adding my keymap & custom serial pin

---
+++ keyboards/splitkb/kyria/keymaps/redcoder09/config.h
@@ -0,0 +1,40 @@
+/* Copyright 2022 Thomas Baart <thomas@splitkb.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// #define RGBLED_SPLIT { 6, 6 }
+#define FORCE_NKRO
+#define STENO_COMBINEDMAP
+#define MIDI_ADVANCED
+#define SERIAL_DEBUG
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+//#define SOFT_SERIAL_PIN GP1 (defined in keyboard.json)
+//#define SPLIT_USB_DETECT
+//#define SPLIT_WATCHDOG_ENABLE
+#define MASTER_LEFT
+#define SPLIT_OLED_ENABLE
+#define OLED_DISPLAY_128X64
+#define ENABLE_RGB_MATRIX_SOLID_COLOR
+#define SPLIT_TRANSPORT_MIRROR
+#define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_MODS_ENABLE
+// #define RGB_MATRIX_DEFAULT_FLAGS LED_FLAG_UNDERGLOW
+//#define PS2_PIO_USE_PIO1
+//#define PS2_CLOCK_PIN D0
+//#define PS2_DATA_PIN  D1
+//#define SPLIT_POINTING_ENABLE
+//#define POINTING_DEVICE_RIGHT
